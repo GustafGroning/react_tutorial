55c9839302b16e8a98994fe44415afb87f3cb8f7
Gustaf Gröning <gustafgroning@gmail.com>
2022-02-15 21:29:57 +0100

    tutorial1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+// sudo chmod +x node_modules/.bin/react-scripts
+import ExpenseItem from "./components/ExpenseItem";
+
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem> </ExpenseItem>
+      
     </div>
   );
 }
